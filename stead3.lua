cc867b1e53339d1a80ac2b96877b68c23c68a435
Peter Kosyh <p.kosyh@gmail.com>
2016-12-17 15:53:09 +0300

    fixes

--- stead3.lua
+++ stead3.lua
@@ -611,7 +611,7 @@ stead.game = stead.class({
 		r = stead.here()
 
 		if state then
-			objs = stead.call(r, 'obj'):look()
+			objs = r.obj:look()
 		end
 		return stead.par(stead.scene_delim, reaction, objs), state
 	end;
@@ -1054,7 +1054,7 @@ stead.method = function(v, n, ...)
 		return v[n], true
 	end
 	if stead.type(v[n]) == 'table' then
-		return v[n](), true
+		return v[n], true
 	end
 	stead.err ("Method not string nor function:"..stead.tostr(n), 2);
 end
