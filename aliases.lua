308edfe261da4eea442753e7b04a89586e8a3835
Peter Kosyh <p.kosyh@gmail.com>
2017-01-14 17:29:59 +0300

    aliases

--- aliases.lua
+++ aliases.lua
@@ -4,3 +4,14 @@ pr = std.pr
 pn = std.pn
 obj = std.obj
 room = std.room
+me = std.me
+here = std.here
+walk = function(...)
+	return std.me():walk(...)
+end
+walkin = function(...)
+	return std.me():walkin(...)
+end
+walkout = function(...)
+	return std.me():walkout(...)
+end
