d5d7af720a8a6ced27c2d1c38888f9b45d71e001
Peter Kosyh <p.kosyh@gmail.com>
2017-01-29 16:03:50 +0300

    sound raw api

--- ext/sound.lua
+++ ext/sound.lua
@@ -1,11 +1,47 @@
+-- raw interface to sound
+
+local std = stead
+
+local snd = std.obj {
+	nam = '@snd';
+}
+
 function instead.get_music()
+	return snd.music, snd.music_loop
+end
+
+function instead.set_music(mus, loop)
+	snd.music = mus
+	snd.loop = std.tonum(loop) or 0
 end
 
 function instead.get_music_fading()
+	return snd.music_fadeout, snd.music_fadein
+end
+
+function instead.finish_music()
+	if (snd.music_loop or 0) == 0 then
+		return false
+	end
+	snd.music_loop = -1
+	return true
 end
 
 function instead.get_sound()
+	return snd.sound, snd.sound_channel, snd.sound_loop
 end
 
-function instead.set_sound()
+function instead.set_sound(sound, chan, loop)
+	snd.sound = sound
+	snd.sound_loop = std.tonum(loop) or 1
+	snd.sound_channel = std.tonum(chan) or -1
 end
+
+std.mod_done(function(s)
+	instead.set_music(nil, -1); -- stom music
+	instead.set_sound('@-1'); -- halt all
+end)
+
+std.mod_cmd(function(s)
+	instead.set_sound(); -- empty sound
+end)
