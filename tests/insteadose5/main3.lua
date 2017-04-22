f31393244d2f12b69305bc60f85fbca11c5d4468
Peter Kosyh <p.kosyh@gmail.com>
2017-04-22 23:11:32 +0300

    about

--- tests/insteadose5/main3.lua
+++ tests/insteadose5/main3.lua
@@ -43,8 +43,8 @@ obj {
 	load = function(s)
 		s.tracks = {
 			'RoccoW_-_01_-_Welcome.ogg',
-			'RoccoW_-_02_-_Echiptian_Swaggah.ogg', 
-			'RoccoW_-_08_-_Sweet_Self_Satisfaction.ogg', 
+			'RoccoW_-_02_-_Echiptian_Swaggah.ogg',
+			'RoccoW_-_08_-_Sweet_Self_Satisfaction.ogg',
 		}
 		table.sort(s.tracks)
 		return s
@@ -85,11 +85,10 @@ obj {
 		instead.restart()
 	end;
 }
-
+game.pic = 'gfx/fractal.gif';
 room {
 	nam = 'main';
-	title = 'ИНСТЕДОЗ 5';
-	pic = 'gfx/fractal.gif';
+	title = 'ИНСТЕДОЗ 5 // ПЯТОЕ ИЗМЕРЕНИЕ';
 	decor = function()
 		pn (fmt.c(fmt.b[[ЖУРНАЛ (СОВЕРШЕННО СЕКРЕТНО)]]))
 		pn ()
@@ -100,7 +99,40 @@ room {
 		pn (fmt.c [[{@game structure|СТРУКТУРА}]]);
 		pn (fmt.c [[{@game i_came_to_myself|Я ОЧНУЛСЯ}]]);
 		pn (fmt.c [[{@game lenochka|ЛЕНОЧКА}]]);
+		pn()
+		pn (fmt.c (fmt.img 'box:200x1,black'))
+		pn()
+		pn (fmt.c [[{#about|Об этом сборнике}]])
 	end;
+}: with {
+	obj {
+		nam = '#about';
+		act = function() walk 'about' end;
+	}
+}
+
+room {
+	nam = 'about';
+	title = 'О сборнике';
+	decor = function()
+		p [[Перед вами сборник приключенческих игр, написанных на движке INSTEAD. Этот сборник составлен
+в рамках мероприятия, которое носит название "ИНСТЕДОЗ".^^
+Время от времени несколько авторов INSTEAD собираются вместе и создают такой сборник небольших игр, объединенных
+общей темой или сюжетом. Темой этого, пятого по счету сборника, стало пятое измерение и путешествие по параллельным
+мирам.^^
+Если вы любите текстовые квесты и научную фантастику -- "Инстедоз" для вас! Играйте и оставляйте отзывы на форуме INSTEAD,
+чтобы авторы могли улучшить свои игры или написать новые.^^
+Если вы хотите попробовать написать свою игру -- для вас есть хорошая новость! Дело в том, что
+"ИНСТЕДОЗ 5" продолжается. Прием работ ведется до конца 2017 года. Так что вы 
+можете сделать этот сборник лучше, написав инетересную историю!^^
+До встречи!^^]];
+		pn(fmt.r (fmt.em "http://instead.syscall.ru"))
+		pn(fmt.r (fmt.em "апрель 2017"))
+		p(fmt.c "{#back|К журналу}")
+	end;
+	obj = {
+		obj { nam = '#back', act = function() walkout() end; };
+	}
 }
 
 function init()
