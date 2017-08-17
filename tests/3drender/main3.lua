613299efd501fb35ebecf62c56dec66078518cec
Peter Kosyh <p.kosyh@gmail.com>
2017-08-17 15:57:01 +0300

    3d test

---
+++ tests/3drender/main3.lua
@@ -0,0 +1,68 @@
+local r = require "render"
+require "sprite"
+require "keys"
+
+function keys:filter(press, key)
+	return key == "left" or key == "right" or key == "w" or key == "a" or key == "s" or key == "d" or key == "up" or key == "down"
+end
+
+global { hangle = 0, vangle = 0, x = 0, y = 0, z = 0 }
+declare { look = false }
+
+game.onkey = function(s, press, key)
+	if not press then
+		return false
+	end
+	local axis
+	if key == "w" then
+		x, y, z = (r.vec3(x, y, z) + look * 2):unpack()
+	elseif key == "a" then
+		x = x - 2
+	elseif key == "s" then	
+		x, y, z = (r.vec3(x, y, z) - look * 2):unpack()
+	elseif key == "d" then
+		x = x + 2
+	elseif key == "right" then
+		hangle = hangle - (math.pi / 32)
+	elseif key == "left" then
+		hangle = hangle + (math.pi / 32)
+	elseif key == "up" then
+		vangle = vangle - (math.pi / 32)
+	elseif key == "down" then
+		vangle = vangle + (math.pi / 32)
+	end
+	scene:camera(x, y, z)
+	look = scene:rotate(hangle, vangle)
+	print(look:unpack())
+--	print("VIEW: ", hangle, vangle)
+	screen:clear(0, 0, 0, 255)
+	scene:render(screen)
+	return true
+end
+
+function gfx()
+	return screen:sprite()
+end
+
+game.pic = gfx
+
+declare {
+	screen = false;
+	scene = false;
+}
+
+function init()
+	screen = pixels.new(640, 480)
+	screen:clear(0, 0, 0, 255)
+	scene = r.scene()
+	local o = r.object():circle(0, 0, 640, { 255, 255, 255, 255 })
+	scene:place(o, 0, 0, 200)
+--	scene:place(o, 0, 0, 5)
+	scene:setfov(160)
+	scene:camera(0, 0, 1)
+	scene:render(screen)
+end
+
+function start()
+	look = scene:rotate(hangle, vangle)
+end
---
+++ tests/3drender/render.lua
@@ -0,0 +1,119 @@
+require "sprite"
+local maf = require "maf"
+local PI = math.pi
+local std = stead
+
+local render = {
+}
+render.__index = render
+
+local scene = {
+}
+scene.__index = scene
+
+local object = {
+}
+object.__index = object
+
+
+function render.object()
+	local o = {
+		shapes = {}
+	}
+	setmetatable(o, object)
+	return o
+end
+
+function object:circle(x, y, r, col)
+	table.insert(self.shapes, { t = 'circle', x = x, y = y, r = r, col = col }) 
+	return self
+end
+
+function object:render(screen, fov, x, y, z)
+	local pos = x
+	if type(x) == 'number' then
+		pos = maf.vec3(x, y, z)
+	end
+	local w, h = screen:size()
+	local xc = math.floor(w / 2)
+	local yc = math.floor(h / 2)
+	for k, o in ipairs(self.shapes) do
+		if o.t == 'circle' and pos.z ~= 0 then
+			local nx = fov * (pos.x + o.x) / pos.z
+			local ny = fov * (pos.y + o.y) / pos.z
+			local nr = (o.r) / pos.z
+			screen:circle(xc + nx, yc - ny, nr, std.unpack(o.col))
+		end
+	end
+end
+
+function render.vec3(x, y, z)
+	return maf.vec3(x, y, z)
+end
+
+function render.scene()
+	local o = {
+		objects = {}
+	}
+	setmetatable(o, scene)
+	o:rotate(0, 0)
+	o:camera(0, 0, 0)
+	o:setfov(PI / 4)
+	return o
+end
+
+function scene:setfov(fov)
+	self.fov = fov
+end
+
+function scene:look(angle, x, y, z)
+	local q = maf.rotation()
+	q:between(maf.vec3(0, 0, 1), x, y, z)
+	self.quat = q
+end
+
+function scene:rotate(hangle, vangle)
+	local qh = maf.rotation()
+	qh:angleAxis(hangle, 0, 1, 0)
+	local qv = maf.rotation()
+	qv:angleAxis(vangle, 1, 0, 0)
+	local qq = maf.rotation()
+	qv:mul(qh, qq)
+	self.quat = qq
+	return qq * maf.vec3(0, 0, 1)
+end
+
+function scene:camera(x, y, z)
+	local coord = x
+	if type(x) == 'number' then
+		coord = maf.vec3(x, y, z)
+	end
+	self.position = coord
+end
+
+function scene:render(screen)
+	local current_scene = {}
+	for k, o in ipairs(self.objects) do
+		local ncoord = (o.pos - self.position):rotate(self.quat)
+		table.insert(current_scene, { o = o.o, pos = ncoord })
+	end
+	table.sort(current_scene, function(a, b)
+		return a.pos.z > b.pos.z
+	end)
+	for k, o in ipairs(current_scene) do
+		if o.pos.z > 0 then
+			o.o:render(screen, self.fov, o.pos)
+			print(o.pos.x, o.pos.y, o.pos.z)
+		end
+	end
+end
+
+function scene:place(object, x, y, z)
+	local coord = x
+	if type(x) == 'number' then
+		coord = maf.vec3(x, y, z)
+	end
+	table.insert(self.objects, { o = object, pos = coord })
+end
+
+return render
