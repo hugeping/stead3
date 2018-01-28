df569ef5a5604e43a8dfe115471fef6dd026fcc5
Peter Kosyh <p.kosyh@gmail.com>
2018-01-28 11:57:49 +0300

    flakes test

--- tests/decor/main3.lua
+++ tests/decor/main3.lua
@@ -3,11 +3,22 @@ loadmod "decor"
 
 function game:timer()
 	D"box".x = D"box".x + 1
+
+	for i = 1, 100 do
+		local d = D("snow"..std.tostr(i))
+		d.y = d.y + rnd(3)
+		d.x = d.x + rnd(3) - 2
+		if d.y > 600 then
+			d.y = 0
+			d.x = rnd(800)
+		end
+	end
+
 	return false
 end
 local text = [[Проверка длинных {test a b|те кстов}, да еще с переносами строк.
 Интересно сработает ли? А также интересно посмотреть на разные типы выравнивания!
-И конечно, на принудительный разрыв строк!]];
+И конечно, на принудительный {test a b|разрыв строк}!]];
 _'main'.dsc = [[Привет мир!]];
 local n = 1
 function game:ondecor(name, a, b, x, y)
@@ -21,7 +32,10 @@ function game:ondecor(name, a, b, x, y)
 	p("click:", name, ":",a, " ", b, " ", x, ",", y)
 end
 function init()
-	timer:set(100)
+	timer:set(50)
+	for i = 1, 100 do
+		decor:new {"snow"..std.tostr(i), "img", "box:4x4,black", x= rnd(800), y = rnd(600), xc = true, yc = true, z = -1 }
+	end
 	decor.bgcol = 'white'
 	decor:new {"box", "img", "box:64x64,red", x= 12, y = 12, xc = true, yc = true }
 --	decor:new {"box2", "img", "box:64x64,blue", x= 320, y = 12, xc = true, yc = true, z = -1 }
